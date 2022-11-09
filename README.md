a84da89589598f6bfed714cebd447d4026c8c145
Wahyu <wahyudinata@gmail.com>
2022-11-08 22:57:21 -0800

    bump version, validate schema during runtime, last step is to publish

--- README.md
+++ README.md
@@ -7,9 +7,9 @@ Welcome! This is repo contains the command line interface to create and publish 
 A column is a plain javascript object that contains how the column would behave once it's mounted to a table on columns.app. Here are the steps to get things started:
 
 - :checkered_flag:. Create a directory, name it whatever you want.
-- :file_folder: Inside the newly created directory run `npx @columnapp/cli init` to bootstrap.
-- :pencil2: Inside you will find `column.ts`, the javascript object exported there is what will be published.
+- :file_folder: Inside the newly created directory run `npx @columnapp/cli init` to bootstrap folder.
+- :pencil2: Inside you will find `column.ts`, the javascript object exported there is what will be published. Currently, we only support typescript.
 - :rocket: Once you are ready to publish, go to https://column.app/publish to get a publish key
 - :star: run `npx @columnapp/cli publish <publish key>`, using the same publish key will
 
-If you have any questions, feel free to email us @ hello@column.app :clap:
+If you have any questions, feel free to email us @ hello@column.app :+1:
--- publish.ts
+++ publish.ts
@@ -2,32 +2,61 @@
 import { Command } from 'commander'
 import colors from 'colors'
 import { exit } from 'process'
-import esbuild from 'esbuild'
+import esbuild, { BuildOptions } from 'esbuild'
+import os from 'os'
+import path from 'path'
+import { readFileSync } from 'fs'
+import { ColumnSchemaCheck, ColumnSchemaError } from '@columnapp/schema'
+type Options = {
+  path?: string
+}
 
-type Options = {}
 const program = new Command()
 program
   .version('0.1.0')
   .argument('[key]', 'publish key, generate one at https://column.app/publish')
-  //   .option('-p, --path', 'path to the column directory, where package.json is at the root')
+  .option('-p, --path <path>', 'path to the column directory, where package.json is at the root')
   .action(async (key: string | null, options: Options) => {
     if (key == null) {
       console.error('Please provide a publish key, you can generate one at https://column.app/publish')
       exit(1)
     }
-    console.log(colors.cyan('Verifying the column...'))
-    console.log(colors.cyan('Packaging the column...'))
-    await esbuild.build({
-      entryPoints: ['column.ts'],
+    const outfile = path.join(os.tmpdir(), `${key}.js`)
+    const cwd = (file?: string) =>
+      path.resolve(path.join(...([options.path == null ? '.' : options.path, file].filter(Boolean) as Array<string>)))
+    const buildOptions = {
+      entryPoints: [cwd('column.ts')],
       allowOverwrite: true,
       bundle: true,
       minify: true,
-      format: 'esm',
-      keepNames: true,
+
       platform: 'browser',
-      outfile: '_column.js',
-    })
-    console.log(colors.cyan('publishing the column...'))
+      absWorkingDir: cwd(),
+      outfile,
+    } as BuildOptions
+    // we need to build twice, once with cjs as platform so we can require and validate, second time using esm to publish
+    // now we build to verify
+    await esbuild.build({ ...buildOptions, format: 'cjs' })
+
+    const column = require(outfile)
+    if (column?.default == null) {
+      console.log(colors.red('Error: unable to find the column schema'))
+      exit(0)
+    }
+    console.log(colors.cyan('Verifying...'))
+    try {
+      delete column.default['version']
+      const validSchema = ColumnSchemaCheck(column.default)
+    } catch (e: any) {
+      if (e instanceof Error) {
+        console.log(colors.red((e as ColumnSchemaError).readable.toString()))
+      } else {
+        console.log(colors.red('Error: unknown error'))
+      }
+      exit(0)
+    }
+    console.log(colors.cyan('Publishing...'))
+    // TODO: publish here
   })
 
 program.parse()
